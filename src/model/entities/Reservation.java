9c322afaebbaaae6e8c8ff0f1d071751a76e16d7
ShadowCrypt7 <teu_email>
2025-07-14 19:53:22 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -42,9 +43,17 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservation dates for update must be future.";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Check-out date must be after check-in date.";
+        }
        this.checkIn = checkIn;
        this.checkOut = checkOut;
+       return null;
     }
 
     @Override
@@ -59,5 +68,4 @@ public class Reservation {
               +duration()
               + " nights";
     }
-
 }
